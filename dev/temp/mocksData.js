7b8cef966a1bda48156b35549e575a71cbc0cade
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-06-06 16:07:13 +0300

    gititnore

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,11 @@
+'head': {
+    defaults: {
+        title: 'book',
+        useSocialMetaTags: false
+    }
+},
+
+__pages: [{
+                name: 'index',
+                href: 'index.html'
+             }]
