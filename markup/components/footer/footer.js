0dbc9d7c3161295a5b07953e267327bebfbe916c
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-06-07 12:49:09 +0300

    add validations

--- markup/components/footer/footer.js
+++ markup/components/footer/footer.js
@@ -1,7 +1,11 @@
 import $ from "jquery";
 import mask from 'jquery-mask-plugin';
+import valid from 'jquery-validation';
+
 
 $(document).ready(function () {
+
+	validate();
 
 	$('.js-ppp').click(function(e){
 		// e.preventDefault();
@@ -23,14 +27,6 @@ $(document).ready(function () {
 		closePpp();
 	});
 
-	// $('.contact__input').on('change', function(){
-	// 	if($(this).val().length){
-	// 		$(this).siblings().css({'display' : 'none'})
-	// 	} else {
-	// 		$(this).siblings().css({'display' : ''});
-	// 	}
-	// });
-
 	$('.contact__input_phone').mask('+000000000000');
 });
 
@@ -49,3 +45,116 @@ $(document).on('click', function(el){
 		closePpp();
 	}
 });
+
+
+function validate() {
+	$('.js-validate').each(function(){
+		if ($(this).length > 0) {
+			$(this).validate({
+				errorClass: 'has-error',
+				rules: {
+					name: {
+						minlength: 2,
+						required: true
+					},
+					city: {
+						minlength: 2
+					},
+					// password: {
+					// 	minlength: 5
+					// },
+					// confirm_password: {
+					// 	minlength: 5,
+					// 	equalTo: '#password'
+					// },
+					email: {
+						email: true,
+						required: true
+					},
+					phone: {
+						minlength: 17,
+						required: true
+					},
+					// sallerTel: {
+					// 	minlength: 17
+					// },
+					address: {
+						minlength: 2
+					},
+					message: {
+						minlength: 4
+					},
+					date: {
+						minlength: 4
+					},
+					time: {
+						minlength: 10
+					},
+					items: {
+						minlength: 1
+					},
+					sallerName: {
+						minlength: 2
+					},
+					sallerPost: {
+						minlength: 2
+					},
+					square: {
+						minlength: 1
+					},
+					garantUserComment:{
+						minlength: 4
+					},
+					client: {
+						required: true
+					},
+					agree: {
+						required: true
+					},
+					obectYstanovki: {
+						required: true
+					},
+					celZakypki: {
+						required: true
+					}
+				},
+				messages: {
+					firstname: '* Вас так зовут?',
+					lastname: '* У вас такая фамилия?',
+					fathername: '* У вас такое отчество?',
+					password: {
+						required: '* Введите пароль',
+						minlength: '* Минимум 5 символов'
+					},
+					confirm_password: {
+						 required: '* Пароли не совпадают',
+						 minlength: '* Минимум 5 символов',
+						 equalTo: '* Пароли не совпадают'
+					},
+					// email: 'заполнено некорректно',
+					address: '* Это Ваш адрес?',
+					any: '* Заполните поле',
+					time: 'Заполните поле',
+					items: 'Заполните поле',
+					company: '* Заполните поле',
+					tel: {
+						required: 'заполнено некорректно',
+						minlength: 'заполнено некорректно'
+					},
+					name: {
+						required: 'заполнено некорректно',
+						minlength: 'Минимум 2 символa'
+					},
+					message: {
+						required: '* Заполните поле',
+						minlength: 'Заполните поле',
+					},
+					text_area: {
+						required: '* Заполните поле',
+						minlength: 'Заполните поле'
+					}
+				}
+			});
+		}
+	});
+}
